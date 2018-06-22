d6c94b5a4bfe471f8eb6a1c8215e2c3a2c995dd2
Jing Chun Tan <jtan1993@gmail.com>
2018-06-22 14:12:15 +0100

    Update EnemyMovement.cs

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,74 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
-    [SerializeField] float runSpeed = 5f;
-    [SerializeField] float jumpSpeed = 5f;
-    [SerializeField] float climbSpeed = 5f;
+    
+    [SerializeField] float moveSpeed = 1f;
     //state
     bool isAlive = true;
+    
 
     //cached component references
-    Rigidbody2D playerRigidBody;
+    Rigidbody2D enemyRigidBody;
     Animator playerAnimator;
     Collider2D playerCollider2D;
     float gravityScaleAtStart;
 
 	// Use this for initialization
 	void Start () {
-        playerRigidBody = GetComponent<Rigidbody2D>();
-        playerAnimator = GetComponent<Animator>();
-        playerCollider2D = GetComponent<Collider2D>();
-        gravityScaleAtStart = playerRigidBody.gravityScale;
-
+	enemyRigidBody = GetComponent<Rigidbody2D>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Run();
-        Climb();
-        Jump();
-        FlipSprite();
+		if(IsFacingRight()){
+				enemyRigidBody.velocity = new Vector2(moveSpeed, 0f);
+		}else{
+			enemyRigidBody.velocity = new Vector2(-moveSpeed, 0f); //move in the negative direction
+		}
+
+    }
+	bool IsFacingRight(){
+		return transform.localScale.x > 0;
 	}
-    private void Jump() {
-        if (!playerCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
-        if (Input.GetButtonDown("Jump")) {
-            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
-            playerRigidBody.velocity += jumpVelocityToAdd;
-        } 
-    }
-
-    private void Run() {
-        float controlThrow = Input.GetAxis("Horizontal"); //value between -1 and 1
-        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, playerRigidBody.velocity.y);
-        playerRigidBody.velocity = playerVelocity;
-
-        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
-        playerAnimator.SetBool("Running", playerHasHorizontalSpeed);
-    }
-
-    private void Climb()
-    {
-        if (!playerCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
-            playerAnimator.SetBool("Climbing", false); //stop the climb animation if exits the state
-	    playerRigidBody.gravityScaleAtStart = gravityScaleAtStart;
-            return;
-        }
-        float controlThrow = Input.GetAxis("Vertical");
-        Vector2 climbVelocity = new Vector2(playerRigidBody.velocity.x, controlThrow * climbSpeed);
-        playerRigidBody.velocity = climbVelocity;
-	playerRigidBody.gravityScaleAtStart = 0f; //zero gravity when the player climbing on a ladder
-		
-        bool playerHasVerticalSpeed = Mathf.Abs(playerRigidBody.velocity.y) > Mathf.Epsilon;
-        playerAnimator.SetBool("Climbing", playerHasVerticalSpeed);
-    }
-
-    private void FlipSprite() {
-        //if the player is moving horizontally reverse the current scaling of x axis
-        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
-        if(playerHasHorizontalSpeed){
-            //reverse the furrent scaling of x asis
-            transform.localScale = new Vector2(Mathf.Sign(playerRigidBody.velocity.x), 1f);
-        }
-
-    }
+	
+	private void OnTriggerExit2D(Collider2D collision){
+		transform.localSclae = new Vector2(-(Mathf.Sign(enemyRigidBody.velocity.x)), 1f);
+	}
 }
