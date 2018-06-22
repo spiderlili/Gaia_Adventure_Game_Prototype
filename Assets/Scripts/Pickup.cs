32398d1fcc0175cdd668edafd216cd5a2473ce30
Jing Chun Tan <jtan1993@gmail.com>
2018-06-22 16:23:31 +0100

    Update Pickup.cs

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour {
 	[SerializeField] AudioClip pickupSFX;
+	[SerializeField] int pointsForPickup = 100;
 	
   private void OnTriggerEnter2D(Collider2D collision){
+    FindObjectOfType<GameSessionManager>().AddToScore(pointsForPickup);
     AudioSource.PlayClipAtPoint(pickupSFX, Camera.main.transform.position);  
     Destroy(gameObject);
 	  
