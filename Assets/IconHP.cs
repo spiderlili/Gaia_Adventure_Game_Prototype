eb46705e1130775e3a1aa6df2efca4c3fc7bb371
Jing <jtan1993@gmail.com>
2018-11-20 04:51:41 +0000

    hearts ui added

---
+++ Assets/IconHP.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconHP : MonoBehaviour {
+    [Range(0.0f, 1.0f)] public float fillAmount;
+    public GameObject hp1;
+    public GameObject hp2;
+    public GameObject hp3;
+    public int lives = 3;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (fillAmount >= 1f / lives * lives) hp3.GetComponent<ShowHeartHP>().showHP = true;
+        else hp3.GetComponent<ShowHeartHP>().showHP = false;
+
+        if (fillAmount >= 1f / lives * lives) hp2.GetComponent<ShowHeartHP>().showHP = true;
+        else hp2.GetComponent<ShowHeartHP>().showHP = false;
+
+        if (fillAmount >= 1f / lives * lives) hp1.GetComponent<ShowHeartHP>().showHP = true;
+        else hp1.GetComponent<ShowHeartHP>().showHP = false;
+    }
+}
---
+++ Assets/ShowHeartHP.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHeartHP : MonoBehaviour {
+    public bool showHP; //whether the HP is full
+    public GameObject heartFull; //shows the full heart if showHP is true 
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (showHP == true)
+        {
+            heartFull.GetComponent<Image>().enabled = true;
+        }
+        else {
+            heartFull.GetComponent<Image>().enabled = false;
+        }
+	}
+}
