40455d3abc243fbfe23d10e5a249a18ea0ec5f58
Jing Tan <jing.tan@mediatonic.co.uk>
2019-04-03 17:01:33 +0100

    added safecheck

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -6,15 +6,23 @@ public class Pickup : MonoBehaviour
 {
     [SerializeField] AudioClip pickupSFX;
     [SerializeField] int pointsForPickup = 100;
+    [SerializeField] private GameSessionManager gameSessionManager;
+
+    private void Start()
+    {
+        gameSessionManager = FindObjectOfType<GameSessionManager>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        FindObjectOfType<GameSessionManager>().AddToScore(pointsForPickup);
+        if (gameSessionManager != null) {
+            FindObjectOfType<GameSessionManager>().AddToScore(pointsForPickup);
+        }
         
         //play SFX at the camera position
         //creates an audio instance at the 3D point of the gameobject, independent of the game object which spawned it.
         AudioSource.PlayClipAtPoint(pickupSFX, Camera.main.transform.position);
-
+        //Debug.Log("Collision Detected");
         Destroy(gameObject);
 
     }
