c286cdecb789d03d59dde2413aae55d4e86e067f
Jing Tan <jing.tan@mediatonic.co.uk>
2019-04-05 14:01:12 +0100

    added fade in/out

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -8,11 +8,13 @@ public class Menu : MonoBehaviour
     public void StartFirstLevel()
     {
         SceneManager.LoadScene(1);
+        StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.Out, 1));
     }
 
     public void LoadMainMenu()
     {
         SceneManager.LoadScene(0);
+        StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.Out, 0));
     }
 
     public void OptionsMenu() {
@@ -26,5 +28,6 @@ public class Menu : MonoBehaviour
     public void WinReplay() {
         FindObjectOfType<GameSessionManager>().DestroyGameSession();
         SceneManager.LoadScene(0);
+        StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.Out, 0));
     }
 }
