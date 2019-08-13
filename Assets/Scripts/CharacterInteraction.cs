3ad54a99a82364bd7c8a3c0b576bfa3c77fdcdb6
Jing Tan <jing.tan@mediatonic.co.uk>
2019-08-13 19:11:41 +0100

    made interactive talk

---
+++ Assets/Scripts/CharacterInteraction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInteraction : MonoBehaviour
+{
+    public float talkRange;
+    private List<TalkInteraction> talkInteractions;
+    //look for all nearby interactive objs - which is closest
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Talk"))
+        {
+            var allInteractions = FindObjectsOfType<TalkInteraction>();
+            foreach (var interaction in allInteractions)
+            {
+                var distance = (interaction.transform.position - this.transform.position).magnitude;
+                if(distance <= talkRange)
+                {
+                    interaction.Talk();
+                    break; //don't hold > once
+                }
+            }
+        }
+    }
+
+    public IEnumerator<TalkInteraction> GetEnumerator()
+    {
+        return talkInteractions.GetEnumerator();    
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, talkRange);
+    }
+
+}
---
+++ Assets/Scripts/TalkInteraction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkInteraction : MonoBehaviour
+{
+   
+    public string NodeName;
+
+    public void Talk()
+    {
+        if (string.IsNullOrEmpty(NodeName) == false)
+        {
+            FindObjectOfType<Yarn.Unity.DialogueRunner>().StartDialogue(NodeName);
+        }
+    }
+
+}
